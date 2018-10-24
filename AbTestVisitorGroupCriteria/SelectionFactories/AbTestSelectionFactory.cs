3d0f9b754ecf6c95a973297a548b712f0a778f3d
Vaibhav-Shimpi <44438694+Vaibhav-Shimpi@users.noreply.github.com>
2018-10-24 14:03:56 +0000

    Update AbTestSelectionFactory.cs
    I have added to filter the active state - AB testing names in the "test" dropdown.

--- AbTestVisitorGroupCriteria/SelectionFactories/AbTestSelectionFactory.cs
+++ AbTestVisitorGroupCriteria/SelectionFactories/AbTestSelectionFactory.cs
@@ -15,7 +15,7 @@ namespace AbTestVisitorGroupCriteria.SelectionFactories
         public IEnumerable<SelectListItem> GetSelectListItems(Type propertyType)
         {
             List<SelectListItem> selectItems = new List<SelectListItem>();
-            var allTests = _marketingTestingWebRepository.Service.GetTestList(new TestCriteria());
+            var allTests = _marketingTestingWebRepository.Service.GetTestList(new TestCriteria()).Where(x => x.State == EPiServer.Marketing.Testing.Core.DataClass.Enums.TestState.Active).ToList();
             foreach(var abTest in allTests)
             {
                 selectItems.Add(new SelectListItem() { Value = abTest.Id.ToString(), Text = abTest.Title });
